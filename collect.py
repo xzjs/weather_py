df2ecb18e0f578d9fad4988c88fa4b7a920821f5
hetanbi <hetanbi@baidu.com>
2018-05-25 16:28:16 +0800

    修改添加数据脚本，使用zmq传输数据

--- collect.py
+++ collect.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python
 # -*- coding:utf-8 -*-
 
-import weather
+import time
+import zmq
 
 if __name__ == '__main__':
+    context = zmq.Context()
+    socket = context.socket(zmq.REQ)
+    socket.connect("tcp://localhost:5556")
     cities = ['101010100', '101020100', '101280101', '101280601', '101120201']
     while True:
         for city in cities:
-            weather.spider(city)
+            socket.send(city)
+            response = socket.recv()
+            print response
             time.sleep(30 * 60)
